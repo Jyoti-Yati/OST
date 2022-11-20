1a0a64e5ad80ca0603a870dec533e5ff2e604478
Jyoti-Yati <115882595+Jyoti-Yati@users.noreply.github.com>
2022-11-20 08:40:59 -0800

    Add files via upload
    python code based on addition and subraction

---
+++ pythoncode.py
@@ -0,0 +1,24 @@
+#python proram to find the addition,subtraction of two numbers.
+
+def addition(n1,n2):
+    result = n1+n2
+    return result
+
+n1=78
+n2=45
+result=addition(n1,n2)
+print("The addition of two number is",result)
+
+addition(n1,n2)
+
+def subtraction(n1,n2):
+    result=n1 - n2
+    return result
+
+num1=3
+num2=89
+result=subtraction(num1,num2)
+print("The subtraction of two number is",result)
+
+
+
